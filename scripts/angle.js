d6e9a10d2cd2a26466e5268f6fcfd4b7e65c01da
nphollon <nhollon@thoughtworks.com>
2013-09-01 16:48:06 -0700

    tidied up angle() namespace

--- scripts/angle.js
+++ scripts/angle.js
@@ -1,22 +1,26 @@
 var angle = function (degrees, minutes, seconds) {
+    var absDegrees, absMinutes, absSeconds;
+    var sign, signedDegrees, signedMinutes, signedSeconds;
+
     var that = {};
 
-    var absDegrees, absMinutes, absSeconds, signedDegrees, signedMinutes, signedSeconds;
 
-    var sign, isNegative, isAnAngle, isEqualMagnitude, hasMalformedSigns;
-
-    hasMalformedSigns = (minutes < 0 && degrees !== 0) ||
-        (seconds < 0 && (minutes !== 0 || degrees !== 0));
-
-    if (hasMalformedSigns) {
-        throw {
-            name: 'ArgumentError',
-            message: 'only the most significant component of an angle may be negative'
-        };
-    }
-
-    isNegative = (degrees < 0) || (minutes < 0) || (seconds < 0);
-    sign = isNegative ? -1 : 1;
+    sign = (function () {
+        var isNegative, hasMalformedSigns;
+        
+        hasMalformedSigns = (minutes < 0 && degrees !== 0) ||
+            (seconds < 0 && (minutes !== 0 || degrees !== 0));
+        
+        if (hasMalformedSigns) {
+            throw {
+                name: 'ArgumentError',
+                message: 'only the most significant component of an angle may be negative'
+            };
+        }
+        
+        isNegative = (degrees < 0) || (minutes < 0) || (seconds < 0);
+        return isNegative ? -1 : 1
+    })();
 
     absDegrees = Math.abs(degrees);
     absMinutes = Math.abs(minutes) || 0;
@@ -25,7 +29,7 @@ var angle = function (degrees, minutes, seconds) {
     signedDegrees = function (appliedSign) {
         appliedSign = appliedSign || 1;
         return appliedSign * sign * absDegrees;
-    }
+    };
 
     signedMinutes = function (appliedSign) {
         appliedSign = appliedSign || 1;
@@ -33,7 +37,7 @@ var angle = function (degrees, minutes, seconds) {
             return appliedSign * sign * absMinutes;
         }
         return absMinutes;
-    }
+    };
 
     signedSeconds = function(appliedSign) {
         appliedSign = appliedSign || 1;
@@ -41,21 +45,8 @@ var angle = function (degrees, minutes, seconds) {
             return appliedSign * sign * absSeconds;
         }
         return absSeconds;
-    }
-
-    isAnAngle = function (object) {
-        return object !== undefined &&
-            object !== null &&
-            typeof object.toDegreesMinutesSeconds === 'function';
-    }
-
-    isEqualMagnitude = function (otherAngle) {
-        var otherDMS = otherAngle.toDegreesMinutesSeconds();
-        return typeof otherDMS === 'object' &&
-            signedDegrees() === otherDMS.degrees &&
-            signedMinutes() === otherDMS.minutes &&
-            signedSeconds() === otherDMS.seconds;
     };
+
 
 
     that.toDegreesMinutesSeconds = function () {
@@ -75,7 +66,21 @@ var angle = function (degrees, minutes, seconds) {
     };
 
     that.equals = function (object) {
-        return isAnAngle(object) && isEqualMagnitude(object);
+        var isAnAngle = function () {
+            return object !== undefined &&
+                object !== null &&
+                typeof object.toDegreesMinutesSeconds === 'function';
+        };
+
+        var isEqualMagnitude = function () {
+            var otherDMS = object.toDegreesMinutesSeconds();
+            return typeof otherDMS === 'object' &&
+                signedDegrees() === otherDMS.degrees &&
+                signedMinutes() === otherDMS.minutes &&
+                signedSeconds() === otherDMS.seconds;
+        };
+        
+        return isAnAngle() && isEqualMagnitude();
     };
 
     that.toString = function () {
@@ -84,7 +89,7 @@ var angle = function (degrees, minutes, seconds) {
             ', minutes: ' + dmsForm.minutes +
             ', seconds: ' + dmsForm.seconds +
             ' }';
-    }
+    };
 
     return that;
 };
