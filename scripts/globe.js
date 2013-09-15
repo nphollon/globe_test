0ce6e2fa6dcd7728eb96b19d9faf8ed79a68a653
nphollon <nhollon@thoughtworks.com>
2013-09-14 18:04:31 -0700

    Consolidate into single js file and single spec.js file

--- scripts/globe.js
+++ scripts/globe.js
@@ -1,3 +1,37 @@
+var argumentValidator = function () {
+    var that = {};
+    var validations = {};
+
+    that.addError = function (name, message, condition) {
+        validations[name] = { message: message, condition: condition };
+    };
+
+    that.validate = function () {
+        forEachProperty(validations, checkErrorCondition);
+    };
+
+    var forEachProperty = function (object, callback) {
+        var property;
+        for (property in object) {
+            if (object.hasOwnProperty(property)) {
+                callback.call(null, object[property]);
+            }
+        }
+    };
+
+    var checkErrorCondition = function (error) {
+        if (error.condition.call()) {
+            throw argumentError(error.message);
+        }        
+    };
+
+    var argumentError = function (message) {
+        return { name: 'ArgumentError', message: message };
+    };
+
+    return that;
+}
+
 var angle = function (degrees, minutes, seconds) {
     var that = {};
 
@@ -172,4 +206,118 @@ var angle = function (degrees, minutes, seconds) {
 
     initialize();
     return that;
+};
+
+var limitedAngle = function (absLimit, name) {
+    var message = (name || 'angle') +
+        ' cannot exceed +/-' + absLimit + ' degrees';
+
+    return function (degrees, minutes, seconds) {
+        var that =  angle(degrees, minutes, seconds);
+
+        if (Math.abs(that.toDecimalDegrees()) > absLimit) {
+            throw {
+                name: 'ArgumentError',
+                message: message
+            };
+        }
+
+        return that;
+    };
+};
+
+var latitude = limitedAngle(90, 'latitude');
+
+var longitude = limitedAngle(180, 'longitude');
+
+var coordinates = function (latDecimal, lonDecimal) {
+    var that = {};
+    var latAngle = latitude(latDecimal);
+    var lonAngle = longitude(lonDecimal);
+
+    var polarLat = 90;
+
+    that.latitude = function () { return latAngle; }
+    that.decLatitude = function () { return latAngle.toDecimalDegrees(); }
+    that.longitude = function () { return lonAngle; };
+    that.decLongitude = function () { return lonAngle.toDecimalDegrees(); }
+
+    that.equals = function (object) {
+        var isCoordinates = function () {
+            return object !== null && object !== undefined && 
+                typeof object.latitude === 'function' &&
+                typeof object.longitude === 'function';
+        };
+
+        var hasEqualComponents = function () {
+            return latAngle.equals(object.latitude()) &&
+                lonAngle.equals(object.longitude());
+        };
+
+        var isAtSamePole = function () {
+            return latAngle.equals(angle(polarLat)) &&
+                object.latitude().equals(angle(polarLat)) ||
+                latAngle.equals(angle(-polarLat)) &&
+                object.latitude().equals(angle(-polarLat));
+        }
+
+        return isCoordinates() && (isAtSamePole() || hasEqualComponents());
+    };
+
+    return that;
+};
+
+"use strict";
+
+var createRenderer = function (context) {
+    var that = {};
+
+    that.drawMarker = function (point) {
+        context.fillRect(point.x() - 5, point.y() - 5, 10, 10);
+    };
+
+    that.drawMarkers = function (points) {
+        points.map(this.drawMarker, this);
+    };
+
+    return that;
+};
+
+var draw = function (canvas) {
+    var renderer = createRenderer(canvas.getContext('2d'));
+    var proj = projection(canvas.width, canvas.height);
+    var coords = healpix.basePixelVertices();
+    var points = coords.map(proj);
+    renderer.drawMarkers(points);
+};
+
+var projection = function (canvasWidth, canvasHeight) {
+    return function (mapCoords) {
+        var x =  canvasWidth / 2 + mapCoords.decLongitude() * canvasWidth / 360;
+        var y =  canvasHeight / 2 - mapCoords.decLatitude() * canvasHeight / 180;
+        return point2d(x, y);
+    };
+};
+
+var point2d = function (x, y) {
+    var that = {};
+    that.x = function () { return x; };
+    that.y = function () { return y; };
+    return that;
+}
+
+var healpix = {};
+
+healpix.basePixelVertices = function () {
+    var vertices = [];
+    var initLongitudes = [180, -135, -90, -135, 180];
+    var i, lon, lat;
+
+    for (i = 0; i < initLongitudes.length; i++) {
+        lat = 90 - i * 45;
+        for (lon = initLongitudes[i]; lon <= 180; lon += 90) {
+            vertices.push(coordinates(lat, lon));
+        }
+    }
+    return vertices;
 };
--- spec/globe_spec.js
+++ spec/globe_spec.js
@@ -170,3 +170,168 @@ describe("Angle", function () {
         });
     });
 });
+
+describe("Latitude", function () {
+    it("should be an angle", function () {
+        var latitudeObject = latitude(1, 2, 3);
+        var angleObject = angle(1, 2, 3);
+
+        expect(angleObject.equals(latitudeObject)).toBeTruthy();
+    });
+
+    it("should be bounded at +/-90 degrees", function () {
+        var error = { message: 'latitude cannot exceed +/-90 degrees' };
+        expect(function () { latitude(90, 0, 1); }).toThrow(error);
+        expect(function () { latitude(-90, 0, 1); }).toThrow(error);
+    });
+});
+
+describe("Longitude", function () {
+    it("should be an angle", function () {
+        var longitudeObject = longitude(1, 2, 3);
+        var angleObject = angle(1, 2, 3);
+
+        expect(angleObject.equals(longitudeObject)).toBeTruthy();
+    });
+
+    it("should be bounded at +/-180 degrees", function () {
+        var error = { message: 'longitude cannot exceed +/-180 degrees' };
+        expect(function () { longitude(180, 0, 1); }).toThrow(error);
+        expect(function () { longitude(-180, 0, 1); }).toThrow(error);
+    });
+});
+
+describe("Coordinates", function() {
+    it("should have a latitude and longitude", function () {
+        var coord = coordinates(1.5, 2.7);
+        expect(angle(1.5).equals(coord.latitude())).toBeTruthy();
+        expect(angle(2.7).equals(coord.longitude())).toBeTruthy();
+    });
+
+    describe("equals()", function () {
+        var coord;
+
+        beforeEach(function () {
+            coord = coordinates(0, 0);
+        });
+
+        it("should be equal to another coordinates with equal lat & long", function () {
+            expect(coord.equals(coordinates(0, 0))).toBeTruthy();
+        });
+
+        it("should be unequal to coordinates with different lat | long", function () {
+            expect(coord.equals(coordinates(1, 0))).toBeFalsy();
+            expect(coord.equals(coordinates(0, 1))).toBeFalsy();
+        });
+
+        it("should be equal if both coordinates are at the same pole", function () {
+            var northPole = coordinates(90, 0);
+            var southPole = coordinates(-90, 0);
+            expect(northPole.equals(coordinates(90, 100))).toBeTruthy();
+            expect(southPole.equals(coordinates(-90, 100))).toBeTruthy();
+        });
+
+        it("should be unequal to things that aren't coordinates", function () {
+            expect(coord.equals(null)).toBeFalsy();
+            expect(coord.equals(undefined)).toBeFalsy();            
+            expect(coord.equals(angle(0))).toBeFalsy();            
+        });
+    });
+});
+
+describe("Renderer", function () {
+    var context;
+    var renderer;
+    var pt;
+
+    beforeEach(function () {
+        context = jasmine.createSpyObj('context', ['fillRect']);
+        renderer = createRenderer(context);
+        pt = point2d(3, 5);
+    });
+
+    describe("drawMarker()", function () {
+        it("should draw a rectangle at coordinates", function () {
+            renderer.drawMarker(pt);
+            expect(context.fillRect).toHaveBeenCalledWith(pt.x() - 5, pt.y() - 5, 10, 10);
+        });
+    });
+
+    describe("drawMarkers()", function () {
+        it("should draw a marker at each of a list of points", function () {
+            renderer.drawMarkers([pt]);
+            expect(context.fillRect).toHaveBeenCalledWith(pt.x() - 5, pt.y() - 5, 10, 10);
+        });
+    });
+});
+
+describe("Equirectangular projection", function () {
+    var plateCaree, verifyProjection;
+
+    verifyProjection = function (mapXY, canvasXY) {
+        var mapCoords = coordinates(mapXY[0], mapXY[1]);
+        var canvasCoords = plateCaree(mapCoords);
+        expect(canvasCoords.x()).toEqual(canvasXY[0]);
+        expect(canvasCoords.y()).toEqual(canvasXY[1]);
+    };
+
+    describe("1px per degree", function () {
+        beforeEach(function () {
+            plateCaree = projection(360, 180);
+        });
+        
+        it("should translate coords (0, 0) to the canvas center", function () {
+            verifyProjection([0, 0], [180, 90]);
+        });
+
+        it("should translate coords (1, 0) to 1px above the canvas center", function () {
+            verifyProjection([1, 0], [180, 89]);
+        });
+
+        it("should translate coords (0, 1) to 1px right of the canvas center", function () {
+            verifyProjection([0, 1], [181, 90]);
+        });
+    });
+
+    describe("0.5px per degree", function () {
+        beforeEach(function () {
+            plateCaree = projection(180, 90);
+        });
+        
+        it("should translate coords (2, 0) to 1px above the canvas center", function () {
+            verifyProjection([2, 0], [90, 44]);
+        });
+
+        it("should translate coords (0, 2) to 1px right of the canvas center", function () {
+            verifyProjection([0, 2], [91, 45]);
+        });
+    });
+});
+
+describe("Healpix", function () {
+    describe("basePixelVertices()", function () {
+        it("should return list of 12 points", function () {
+            var expectedCoords = [
+                coordinates(90, 0),
+                coordinates(45, -135),
+                coordinates(45, -45),
+                coordinates(45, 45),
+                coordinates(45, 135),
+                coordinates(0, -90),
+                coordinates(0, 0),
+                coordinates(0, 90),
+                coordinates(0, 180),
+                coordinates(-45, -135),
+                coordinates(-45, -45),
+                coordinates(-45, 45),
+                coordinates(-45, 135),
+                coordinates(-90, 0),
+            ];
+            var actualCoords = healpix.basePixelVertices();
+            var i;
+            for (i in expectedCoords) {
+                expect(expectedCoords[i].equals(actualCoords[i])).toBeTruthy();
+            }
+        });
+    });
+});
